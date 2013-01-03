9525037f5435cbce1dda31025e61f516224aed9c
Josh Marshall <catchjosh@gmail.com>
2013-01-02 23:48:11 -0600

    Recursing asynchronously != asynchronous recursion, and closures can be confusing during BSG immersion.

--- dopy/tree.py
+++ dopy/tree.py
@@ -10,6 +10,49 @@ class Tree(object):
         self._registrations[name] = ("function", function)
 
     def call_method(self, method, callback, *args, **kwargs):
-        callee_type, callee = self._registrations[method]
-        result = callee(*args, **kwargs)
+        # this is really ugly, especially when we need to make the
+        # "@identifier" lookup asynchronous
+        callee_type, callee = self._registrations.get(method, (None, None))
+        if callee:
+            # the exact function was registered, so call it
+            _call_function(callee, args, kwargs, callback)
+        else:
+            method_parts = method.split(".")
+            method_name = method_parts.pop(0)
+            callee_type, callee = self._registrations[method_name]
+            _crawl_attributes(method_parts, callee, args, kwargs, callback)
+
+
+def _crawl_attributes(method_parts, callee, args, kwargs, callback):
+    while method_parts:
+        attribute_name = method_parts.pop(0)
+        if attribute_name.startswith("@"):
+            def lookup_callback(result):
+                _crawl_attributes(method_parts, result, args, kwargs, callback)
+            arguments = (attribute_name[1:],)
+            return _call_function(callee, arguments, {}, lookup_callback)
+        callee = getattr(callee, attribute_name)
+    _call_function(callee, args, kwargs, callback)
+
+
+def _call_function(callee, args, kwargs, callback):
+    """Call a function (asynchronously if indicated) and pass the result
+    to the callback.
+
+    """
+    async = getattr(callee, "_auto_finish", True) is False
+    if async:
+        kwargs["callback"] = callback
+    result = callee(*args, **kwargs)
+    if not async:
         callback(result)
+
+
+def asynchronous(method):
+    """Wrap a function or method so that the tree knows not to use
+    the return value.
+
+    """
+    # TODO -- write tests and ExceptionStackContext stuff.
+    method._auto_finish = False
+    return method
---
+++ tests/test_tree.py
@@ -0,0 +1,91 @@
+from dopy.tree import Tree, asynchronous
+import mock
+import unittest
+
+
+class Foo(object):
+    """Mock class for testing instances, classmethods, etc."""
+
+    def __init__(self, identifier=None):
+        self.identifier = identifier
+
+    @classmethod
+    def class_method(klass, *args, **kwargs):
+        return (klass, args, kwargs)
+
+    def instance_method(self, *args, **kwargs):
+        return (self, args, kwargs)
+
+    @classmethod
+    @asynchronous
+    def async_class_method(klass, *args, **kwargs):
+        callback = kwargs.pop("callback")
+        callback((klass, args, kwargs))
+
+    @asynchronous
+    def async_instance_method(self, *args, **kwargs):
+        callback = kwargs.pop("callback")
+        callback((self, args, kwargs))
+
+
+class TestTree(unittest.TestCase):
+
+    def setUp(self):
+        # Mock functions
+        self._mock_bar = mock.Mock()
+        self._mock_foo = mock.Mock()
+        self._mock_bar.return_value = self._mock_foo
+        self._mock_async = asynchronous(self.get_async_function())
+
+        # registering with the tree
+        self._tree = Tree()
+        self._tree.register_class("foo", Foo)
+        self._tree.register_function("bar", self._mock_bar)
+        self._tree.register_function("async", self._mock_async)
+
+    def call_method(self, method, *args, **kwargs):
+        results = []
+
+        def callback(result):
+            results.append(result)
+        self._tree.call_method(method, callback, *args, **kwargs)
+        self.assertEqual(1, len(results))
+        return results[0]
+
+    def get_async_function(self):
+        def async_function(*args, **kwargs):
+            self.assertTrue("callback" in kwargs)
+            callback = kwargs.pop("callback")
+            callback((args, kwargs))
+        return async_function
+
+    def test_tree_function(self):
+        self.call_method("bar", 5, 6)
+        self._mock_bar.assert_called_with(5, 6)
+
+    def test_tree_function_with_keyword_arguments(self):
+        self.call_method("bar", foo="bar")
+        self._mock_bar.assert_called_with(foo="bar")
+
+    def test_tree_function_asynchronous(self):
+        result = self.call_method("async", foo="bar")
+        self.assertEqual(((), {"foo": "bar"}), result)
+
+    def test_tree_class(self):
+        result = self.call_method("foo")
+        self.assertTrue(isinstance(result, Foo))
+        self.assertEqual(result.identifier, None)
+
+    def test_tree_class_method(self):
+        result = self.call_method("foo.class_method", foo="bar")
+        self.assertEqual((Foo, (), {"foo": "bar"}), result)
+
+    def test_tree_class_method_asynchronous(self):
+        result = self.call_method("foo.async_class_method", foo="bar")
+        self.assertEqual((Foo, (), {"foo": "bar"}), result)
+
+    def test_tree_instance(self):
+        instance, args, kwargs = self.call_method("foo.@shoe.instance_method")
+        self.assertEqual((), args)
+        self.assertEqual({}, kwargs)
+        self.assertEqual("shoe", instance.identifier)
